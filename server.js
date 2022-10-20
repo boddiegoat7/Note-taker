1f0e124a01e4ad97ec2e23b03ec320eae224ff30
victor boddie <victorboddie21@gmail.com>
2022-10-20 15:35:39 -0400

    added const and required packages to server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const fs = require("fs");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+
+app.listen(PORT, () => {
+    console.log(`API server on port http://localhost:${PORT}`);
+});
